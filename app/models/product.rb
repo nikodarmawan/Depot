bae339f39bb2df7eb5d58b9e9065c0388222bc5a
Niko Darmawan <nikoandhikadarmawan@192-168-1-100.tpgi.com.au>
2011-05-09 00:36:39 +1000

    First Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+    validates :title, :description, :image_url, :presence => true
+    validates :price, :numericality => {:greater_than_or_equal => 0.01 }
+    validates :title, :uniqueness => true
+    validates :image_url, :format =>{
+        :with => %r{\.(gif|jpg|png|jpeg)}i,
+        :message => "must be URL for GIF,JPG,JPEG, or PNG image."
+    }
 end
